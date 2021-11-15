0b4b9346fedd94c7e28a68af8787a602385379c4
mohammad-miftakhus-sholikin <mohammadmiftakhussholikin@gmail.com>
2021-11-15 12:27:57 +0700

    Signed-off-by: mohammad-miftakhus-sholikin <mohammadmiftakhussholikin@gmail.com>

--- student-exam-scoring/student-exam-scoring.py
+++ student-exam-scoring/student-exam-scoring.py
@@ -1,34 +1,60 @@
+## Import some packages
 import PySimpleGUI as sg
 import pandas as pd
 
-# Add some color to the window
-sg.theme('DarkTeal9')
 
+## Add some color to the window
+sg.theme('BlueMono')
+
+
+## Input excel file (note: this file must same in name)
 EXCEL_FILE = 'student-exam-score.xlsx'
 df = pd.read_excel(EXCEL_FILE)
 
+
+## Adding form input and output
+
+# example code of input and output layout
+# layout = [
+    # [sg.Text('Please fill out the following fields:')],
+    # [sg.Text('Name', size=(15,1)), sg.InputText(key='Name')],
+    # [sg.Text('City', size=(15,1)), sg.InputText(key='City')],
+    # [sg.Text('Favorite Colour', size=(15,1)), sg.Combo(['Green', 'Blue', 'Red'], key='Favorite Colour')],
+    # [sg.Text('I speak', size=(15,1)),
+                            # sg.Checkbox('German', key='German'),
+                            # sg.Checkbox('Spanish', key='Spanish'),
+                            # sg.Checkbox('English', key='English')],
+    # [sg.Text('No. of Children', size=(15,1)), sg.Spin([i for i in range(0,16)],
+                                                       # initial_value=0, key='Children')],
+    # [sg.Submit(), sg.Button('Clear'), sg.Exit()]
+# ]
+
+# input and output layout
 layout = [
-    [sg.Text('Please fill out the following fields:')],
+    # form header
+    [sg.Text('Form to enter exam scores:')],
+    # adding no
+    [sg.Text('No', size=(15,1)), sg.InputText(key='No')],
+    # adding Student_number
+    [sg.Text('Student_number', size=(15,1)), sg.InputText(key='Student_number')],
+    # adding Name
     [sg.Text('Name', size=(15,1)), sg.InputText(key='Name')],
-    [sg.Text('City', size=(15,1)), sg.InputText(key='City')],
-    [sg.Text('Favorite Colour', size=(15,1)), sg.Combo(['Green', 'Blue', 'Red'], key='Favorite Colour')],
-    [sg.Text('I speak', size=(15,1)),
-                            sg.Checkbox('German', key='German'),
-                            sg.Checkbox('Spanish', key='Spanish'),
-                            sg.Checkbox('English', key='English')],
-    [sg.Text('No. of Children', size=(15,1)), sg.Spin([i for i in range(0,16)],
-                                                       initial_value=0, key='Children')],
+    # adding Score
+    [sg.Text('Score', size=(15,1)), sg.InputText(key='Score')],
+    # submit and exit button
     [sg.Submit(), sg.Button('Clear'), sg.Exit()]
 ]
 
-window = sg.Window('Simple data entry form', layout)
+## Define title and layout
+window = sg.Window('From for students exam scores', layout)
 
+## If has empty values
 def clear_input():
     for key in values:
         window[key]('')
     return None
 
-
+## Define functional button
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
